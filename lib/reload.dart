cd5ec8e95a8a21ebe7f7523e48355fbbdc727ecf
Benjamin Cherry <ben@devcarbon.com>
2022-12-06 17:18:28 -0900

    Works!!!   Problem does indeed lie in the need to mark for repaint.

---
+++ lib/reload.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+void rebuildAllChildren(BuildContext context) {
+  void rebuild(Element el) {
+    el.markNeedsBuild();
+    el.visitChildren(rebuild);
+  }
+  (context as Element).visitChildren(rebuild);
+}
